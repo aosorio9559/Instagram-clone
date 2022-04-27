3b58d2314def94130dd43379c16f8bc0afcb6e25
aosorio9559 <aosorio9559@gmail.com>
2022-04-26 23:17:26 -0500

    Add single photo component

--- src/App.js
+++ src/App.js
@@ -3,19 +3,24 @@ import { ListOfCategories } from "./components/ListOfCategories/ListOfCategories
 import { GlobalStyle } from "./styles/GlobalStyles";
 import { Logo } from "./components/Logo/Logo";
 import { ListOfPhotoCardsContainer } from "./container/ListOfPhotoCardsContainer";
+import { PhotoCardWithQuery } from "./container/PhotoCardWithQuery";
 
 export const App = () => {
-  const urlParams = new URLSearchParams(location.search);
+  const urlParams = new URLSearchParams(window.location.search);
   const detailId = urlParams.get("detail");
-
-  console.log(detailId);
 
   return (
     <>
       <GlobalStyle />
       <Logo />
-      <ListOfCategories />
-      <ListOfPhotoCardsContainer categoryId={3} />
+      {detailId ? (
+        <PhotoCardWithQuery id={detailId} />
+      ) : (
+        <>
+          <ListOfCategories />
+          <ListOfPhotoCardsContainer categoryId={1} />
+        </>
+      )}
     </>
   );
 };
