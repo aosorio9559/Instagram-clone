f5cc33ce3a30d36f619d69c2f0b5c7bb2731db95
aosorio9559 <aosorio9559@gmail.com>
2022-05-04 17:19:30 -0500

    Persist user session

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -3,8 +3,14 @@ import React, { createContext, useState } from "react";
 const AppContext = createContext(null);
 
 const AppProvider = ({ children }) => {
-  const [isUserLogged, setIsUserLogged] = useState(false);
-  const activateAuth = () => setIsUserLogged(true);
+  const [isUserLogged, setIsUserLogged] = useState(() => {
+    return window.sessionStorage.getItem("token");
+  });
+
+  const activateAuth = (token) => {
+    setIsUserLogged(true);
+    window.sessionStorage.setItem("token", token);
+  };
 
   return (
     <AppContext.Provider value={{ isUserLogged, activateAuth }}>
