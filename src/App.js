fd9d4e5ccd5b9779f7f95ea738ce945cf05c122c
aosorio9559 <aosorio9559@gmail.com>
2022-05-05 12:02:29 -0500

    Add Not found page logic

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 import { GlobalStyle } from "./styles/GlobalStyles";
 import { Logo } from "./components/Logo/Logo";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Detail } from "./pages/Detail";
 import { NavBar } from "./components/NavBar/NavBar";
@@ -9,6 +9,7 @@ import { Favs } from "./pages/Favs";
 import { User } from "./pages/User";
 import { NotRegisteredUser } from "./pages/NotRegisteredUser";
 import { AppContext } from "./context/AppContext";
+import { NotFound } from "./pages/NotFound";
 
 export const App = () => {
   const { isUserLogged } = useContext(AppContext);
@@ -18,18 +19,27 @@ export const App = () => {
       <GlobalStyle />
       <Logo />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/pet/:categoryId" element={<Home />} />
-        <Route path="/detail/:detailId" element={<Detail />} />
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/pet/:categoryId" element={<Home />} />
+        <Route exact path="/detail/:detailId" element={<Detail />} />
         <Route
+          exact
           path="/favs"
-          element={isUserLogged ? <Favs /> : <NotRegisteredUser />}
+          element={isUserLogged ? <Favs /> : <Navigate replace to="/login" />}
         />
         <Route
+          exact
           path="/user"
-          element={isUserLogged ? <User /> : <NotRegisteredUser />}
+          element={isUserLogged ? <User /> : <Navigate replace to="/login" />}
         />
-        {/* <Route path="/register" component={NotRegisteredUser} /> */}
+        <Route
+          exact
+          path="/login"
+          element={
+            !isUserLogged ? <NotRegisteredUser /> : <Navigate replace to="/" />
+          }
+        />
+        <Route path="*" element={<NotFound />} />
       </Routes>
       <NavBar />
     </BrowserRouter>
