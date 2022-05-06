dcfc22f0306bbe09451ee43d4db87cc10458a105
aosorio9559 <aosorio9559@gmail.com>
2022-05-06 00:30:53 -0500

    Make app a PWA

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifestPlugin = require("webpack-pwa-manifest");
+const path = require("path");
+const WorkboxWebpackPlugin = require("workbox-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -9,6 +12,44 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "src/index.html",
+    }),
+    new WebpackPwaManifestPlugin({
+      name: "Pet photos",
+      short_name: "Pet photos",
+      description: "Your pet app",
+      background_color: "#fff",
+      theme_color: "#fff",
+      // start_url: "/",
+      // scope: "/",
+      icons: [
+        {
+          src: path.resolve("src/assets/icon.png"),
+          sizes: [96, 128, 192, 256, 384, 512],
+          purpose: "any maskable",
+        },
+      ],
+    }),
+    new WorkboxWebpackPlugin.GenerateSW({
+      runtimeCaching: [
+        {
+          urlPattern: new RegExp(
+            "https://(res.cloudinary.com|images.unsplash.com)"
+          ),
+          handler: "CacheFirst",
+          options: {
+            cacheName: "images",
+          },
+        },
+        {
+          urlPattern: new RegExp(
+            "https://petgram-server-aosorio9559.vercel.app"
+          ),
+          handler: "NetworkFirst",
+          options: {
+            cacheName: "api",
+          },
+        },
+      ],
     }),
   ],
   module: {
