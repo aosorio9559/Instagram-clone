c15b219cd946c683e3f01c1bf15f663b7fca0ed7
aosorio9559 <aosorio9559@gmail.com>
2022-05-05 22:44:58 -0500

    Add Lazy & Suspense to load pages on demand

--- src/App.js
+++ src/App.js
@@ -1,51 +1,69 @@
-import React, { useContext } from "react";
+import React, { Suspense, useContext } from "react";
 import { GlobalStyle } from "./styles/GlobalStyles";
 import { Logo } from "./components/Logo/Logo";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import { Home } from "./pages/Home";
-import { Detail } from "./pages/Detail";
 import { NavBar } from "./components/NavBar/NavBar";
-import { Favs } from "./pages/Favs";
-import { User } from "./pages/User";
 import { NotRegisteredUser } from "./pages/NotRegisteredUser";
 import { AppContext } from "./context/AppContext";
-import { NotFound } from "./pages/NotFound";
+
+/* Using React Lazy to load pages on demand only */
+const Detail = React.lazy(() =>
+  import("./pages/Detail").then((module) => ({ default: module.Detail }))
+);
+const Favs = React.lazy(() =>
+  import("./pages/Favs").then((module) => ({ default: module.Favs }))
+);
+const Home = React.lazy(() =>
+  import("./pages/Home").then((module) => ({ default: module.Home }))
+);
+const User = React.lazy(() =>
+  import("./pages/User").then((module) => ({ default: module.User }))
+);
+const NotFound = React.lazy(() =>
+  import("./pages/NotFound").then((module) => ({ default: module.NotFound }))
+);
 
 export const App = () => {
   const { isUserLoggedIn } = useContext(AppContext);
 
   return (
-    <BrowserRouter>
-      <GlobalStyle />
-      <Logo />
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/pet/:categoryId" element={<Home />} />
-        <Route exact path="/detail/:detailId" element={<Detail />} />
-        <Route
-          exact
-          path="/favs"
-          element={isUserLoggedIn ? <Favs /> : <Navigate replace to="/login" />}
-        />
-        <Route
-          exact
-          path="/user"
-          element={isUserLoggedIn ? <User /> : <Navigate replace to="/login" />}
-        />
-        <Route
-          exact
-          path="/login"
-          element={
-            !isUserLoggedIn ? (
-              <NotRegisteredUser />
-            ) : (
-              <Navigate replace to="/" />
-            )
-          }
-        />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-      <NavBar />
-    </BrowserRouter>
+    <Suspense fallback={<div>Loading...</div>}>
+      <BrowserRouter>
+        <GlobalStyle />
+        <Logo />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/pet/:categoryId" element={<Home />} />
+          <Route exact path="/detail/:detailId" element={<Detail />} />
+          <Route
+            exact
+            path="/favs"
+            element={
+              isUserLoggedIn ? <Favs /> : <Navigate replace to="/login" />
+            }
+          />
+          <Route
+            exact
+            path="/user"
+            element={
+              isUserLoggedIn ? <User /> : <Navigate replace to="/login" />
+            }
+          />
+          <Route
+            exact
+            path="/login"
+            element={
+              !isUserLoggedIn ? (
+                <NotRegisteredUser />
+              ) : (
+                <Navigate replace to="/" />
+              )
+            }
+          />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        <NavBar />
+      </BrowserRouter>
+    </Suspense>
   );
 };
